d03dc20b34d37843a08cb3cf137fe42bf840ac97
dave8git <dave8git@gmail.com>
2025-05-01 20:14:07 +0200

    add index.html and functions.js added

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+function printMessage(mst) {
+    let div = document.createElement('div');
+    div.innerHTML = msg;
+    document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages() {
+    document.getElementById('messages').innerHTML = '';
+}
